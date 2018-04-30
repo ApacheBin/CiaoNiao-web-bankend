37c660a1dfe7ffdbff4ac17dd529b016e63ce92b
DFFuture <albert.zhang23@gmail.com>
2018-04-30 22:57:30 +0800

    1. 添加Swagger

--- src/main/java/com/cainiaoshixi/controller/WorkController.java
+++ src/main/java/com/cainiaoshixi/controller/WorkController.java
@@ -5,6 +5,9 @@ import com.cainiaoshixi.entity.Work;
 import com.cainiaoshixi.service.IWorkService;
 import com.cainiaoshixi.util.ResultUtil;
 import com.cainiaoshixi.util.SessionUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/work")
 @CrossOrigin
+@Api(value = "工作经历控制器", tags = {"实习/工作经验接口"})
 @ResponseBody
 public class WorkController {
 
@@ -25,15 +29,18 @@ public class WorkController {
         this.session = session;
     }
 
-    @RequestMapping("/get")
-    public Result getWork(@RequestParam(value = "id", required = false) Integer id) {
+    @RequestMapping(value = "/get", method = RequestMethod.POST)
+    @ApiOperation("获取工作经验")
+    public Result getWork(@ApiParam(name = "id", value = "工作经验ID") Integer id) {
         Integer userId = session.userId();
-        return getWorkById(id);
+        return ResultUtil.success(getWorkById(id));
 
     }
 
-    @RequestMapping("/get/{id}")
-    public Result getWorkById(@PathVariable Integer id) {
+    @GetMapping("/get/{id}")
+    @ApiOperation("根据ID获取工作经验")
+    public Result getWorkById(@ApiParam(name = "id", value = "工作经验ID", required = true)
+                                  @PathVariable("id") Integer id) {
         return ResultUtil.success(workService.getById(id));
     }
 }
