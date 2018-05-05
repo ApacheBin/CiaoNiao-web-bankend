fbf8fc33cf1b51e85dabf52619b9daab03e0698e
DFFuture <albert.zhang23@gmail.com>
2018-05-05 12:53:19 +0800

    1. 暂时关闭统一异常处理，方便查错

--- src/main/java/com/cainiaoshixi/exception/CnExceptionHandler.java
+++ src/main/java/com/cainiaoshixi/exception/CnExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ControllerAdvice
+//@ControllerAdvice
 public class CnExceptionHandler {
     private final static Logger logger = LoggerFactory.getLogger(CnExceptionHandler.class);
 
