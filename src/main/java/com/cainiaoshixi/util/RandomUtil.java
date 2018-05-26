6c2a75801a0173e5036598f2a900b8d2c76f68b8
DFFuture <albert.zhang23@gmail.com>
2018-05-27 02:18:53 +0800

    生成二维码

--- src/main/java/com/cainiaoshixi/util/RandomUtil.java
+++ src/main/java/com/cainiaoshixi/util/RandomUtil.java
@@ -1,6 +1,19 @@
 package com.cainiaoshixi.util;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.UUID;
 
 /**
  * 随机数工具类
@@ -48,5 +61,16 @@ public class RandomUtil {
             }
         else
             return (char)('0' + random.nextInt(10));
+    }
+
+    public static void generateQRcode( String content, OutputStream out) throws WriterException, IOException {
+        int width = 150;
+        int height = 150;
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        hints.put(EncodeHintType.MARGIN, 2);
+        BitMatrix matrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, width, height, hints);
+        MatrixToImageWriter.writeToStream(matrix, "png", out);
     }
 }
