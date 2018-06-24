f3e3b04c52ab13f066e9958333235e1cbd7f979e
RainBrother <cason_chenhanyu@163.com>
2018-06-24 23:33:36 +0800

    扫码登录

--- src/main/java/com/cainiaoshixi/controller/web/LoginController.java
+++ src/main/java/com/cainiaoshixi/controller/web/LoginController.java
@@ -1,8 +1,11 @@
 package com.cainiaoshixi.controller.web;
 
+import com.alibaba.fastjson.JSON;
 import com.cainiaoshixi.domain.Result;
 import com.cainiaoshixi.util.RedisUtil;
 import com.cainiaoshixi.util.ResultUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,19 +21,36 @@ import javax.servlet.http.HttpServletRequest;
 
 @CrossOrigin
 @Controller
+@RequestMapping("/scan")
 public class LoginController {
     @Autowired
     private RedisUtil redisUtil;
+    private static final long EXPIRE_TIME = 86400; //sessionId有效时间，以秒为单位
+    private static final String LOG_TITLE = "WEB_LoginController";
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    private static final long expireTime = 86400; //sessionId有效时间，以秒为单位
-
-    @RequestMapping("/scanToLogin")
+    @RequestMapping("/login")
     public Result scanToLogin(HttpServletRequest request) {
         int userId = (int) request.getSession().getAttribute("userId");
         String token = request.getParameter("token");
+        logger.info(LOG_TITLE + "scanToLogin, userId: {}, token: {}", userId, token);
         if (token == null || token.isEmpty())
             return ResultUtil.error(-201, "token为空");
-        redisUtil.set(token, userId + "", expireTime);
+        redisUtil.set(token, userId + "", EXPIRE_TIME);
         return ResultUtil.success("登录成功");
+    }
+
+    @RequestMapping("/checkStatus")
+    public Result checkLoginStatus(String token) {
+        if (token == null || token.isEmpty())
+            return ResultUtil.error(-201, "token为空");
+        Integer userId = (Integer) redisUtil.get(token);
+        logger.info(LOG_TITLE + "checkLoginStatus, userId: {}, token: {}", userId, token);
+        if (userId == null) {
+            return ResultUtil.error(-301, "unLogin");
+        }else {
+            redisUtil.expire(token, EXPIRE_TIME);
+            return ResultUtil.success("login success");
+        }
     }
 }
