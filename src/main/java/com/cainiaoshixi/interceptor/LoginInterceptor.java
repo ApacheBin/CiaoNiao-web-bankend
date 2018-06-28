c916008a0806c8cb17a399a2343d1ff215d8671e
RainBrother <cason_chenhanyu@163.com>
2018-06-28 21:37:48 +0800

    扫码登录

--- src/main/java/com/cainiaoshixi/interceptor/LoginInterceptor.java
+++ src/main/java/com/cainiaoshixi/interceptor/LoginInterceptor.java
@@ -2,7 +2,6 @@ package com.cainiaoshixi.interceptor;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.cainiaoshixi.controller.web.ScanController;
 import com.cainiaoshixi.domain.Result;
 import com.cainiaoshixi.util.RedisUtil;
 import com.cainiaoshixi.util.ResultUtil;
